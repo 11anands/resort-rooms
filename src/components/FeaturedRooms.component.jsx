16857fd28178ee36d48dc7d5383200116597c40b
Anand Sharan <anandsharan97@gmail.com>
2021-01-10 20:04:59 +0100

    featuredRooms

--- src/components/FeaturedRooms.component.jsx
+++ src/components/FeaturedRooms.component.jsx
@@ -4,7 +4,8 @@ import { RoomContext } from '../context';
 export default class FeaturedRooms extends Component {
     static contextType= RoomContext;
     render() {
-        const { name, greeting } = this.context;
+        const { featuredRooms:rooms } = this.context;
+        console.log(rooms);
         return (
             <div>
               Hello from featuredrooms 
